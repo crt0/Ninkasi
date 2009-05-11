fe0fc05080af73436fabcd04a82badca0b090408
Andrew Korty <andrew@korty.name>
2009-05-11 18:45:10 +0000

    move category name/number hashes here

--- lib/Ninkasi/Category.pm
+++ lib/Ninkasi/Category.pm
@@ -15,4 +15,42 @@ CREATE TABLE category (
 )
 EOF
 
+
+my @category_names = (
+    'none',
+    'Light Lager',
+    'Pilsner',
+    'European Amber Lager',
+    'Dark Lager',
+    'Bock',
+    'Light Hybrid Beer',
+    'Amber Hybrid Beer',
+    'English Pale Ale',
+    'Scottish and Irish Ale',
+    'American Ale',
+    'English Brown Ale',
+    'Porter',
+    'Stout',
+    'India Pale Ale (IPA)',
+    'German Wheat and Rye Beer',
+    'Belgian and French Ale',
+    'Sour Ale',
+    'Belgian Strong Ale',
+    'Strong Ale',
+    'Fruit Beer',
+    'Spice/Herb/Vegetable Beer',
+    'Smoke-Flavored and Wood-Aged Beer',
+    'Specialty Beer',
+    'Unhopped Beer',
+);
+
+our @CATEGORIES = ();
+foreach my $number (0..$#category_names) {
+    push @CATEGORIES, {
+        field_name => sprintf('category%02d', $number),
+        name       => $category_names[$number],
+        number     => $number,
+    };
+}
+
 1;
