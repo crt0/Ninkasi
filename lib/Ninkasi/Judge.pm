ab29ee24836a3af677fb684cc1132e16f38ca791
Andrew Korty <andrew@korty.name>
2009-05-11 18:45:33 +0000

    Support new way of storing availability: set the category in the assignment table to 0.  (-1 or no row means unavailable).

--- lib/Ninkasi/Judge.pm
+++ lib/Ninkasi/Judge.pm
@@ -8,8 +8,7 @@ use base 'Ninkasi::Table';
 __PACKAGE__->Table_Name('judge');
 __PACKAGE__->Column_Names(qw/first_name last_name address city state zip
                              phone_evening phone_day email rank bjcp_id
-                             flight1 flight2 flight3 competitions_judged
-                             pro_brewer when_created/);
+                             competitions_judged pro_brewer when_created/);
 __PACKAGE__->Create_Sql(<<'EOF');
 CREATE TABLE judge (
     first_name          TEXT,
@@ -23,9 +22,6 @@ CREATE TABLE judge (
     email               TEXT,
     rank                INTEGER,
     bjcp_id             TEXT,
-    flight1             INTEGER,
-    flight2             INTEGER,
-    flight3             INTEGER,
     competitions_judged INTEGER,
     pro_brewer          INTEGER,
     when_created        INTEGER
