2f02e3b990cf13d7d353c6228ba5a124f5cecb37
Andrew Korty <andrew@korty.name>
2010-02-15 21:06:21 +0000

    add documentation template -- fill in later

--- lib/Ninkasi.pm
+++ lib/Ninkasi.pm
@@ -7,3 +7,79 @@ use warnings;
 our $VERSION = '0.01';
 
 1;
+__END__
+
+=head1 NAME
+
+Ninkasi - web application to automate judge registration for BCJP competitions
+
+=head1 VERSION
+
+This documentation refers to Ninkasi version 0.0.1.
+
+=head1 SYNOPSIS
+
+
+
+=head1 DESCRIPTION
+
+Ninkasi is a web-based judge registration system for BJCP homebrew and
+professional beer competitions.  The Ninkasi module currently also
+builds the Brewers' Cup web site.
+
+=head1 SUBROUTINES/METHODS
+
+
+
+=head1 DIAGNOSTICS
+
+
+
+=head1 CONFIGURATION
+
+
+
+=head1 ENVIRONMENT
+
+
+
+=head1 DEPENDENCIES
+
+
+
+=head1 INCOMPATIBILITIES
+
+
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs in this module.  Please report problems to
+Andrew Korty <ajk@iu.edu>.  Patches are welcome.
+
+=head1 AUTHOR
+
+Andrew Korty <ajk@iu.edu>
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is in the public domain.
+
+=head1 EXAMPLES
+
+
+
+=head1 FREQUENTLY ASKED QUESTIONS
+
+
+
+=head1 COMMON USAGE MISTAKES
+
+
+
+=head1 ACKNOWLEDGMENTS
+
+
+
+=head1 SEE ALSO
+
+
